773711e910a3077209344fdaa18f64aa29ecdfbe
remon <blackshadowsoftwareltd@gmail.com>
2023-01-21 23:46:08 +0600

    access some types of variable

--- constructors/bin/main.dart
+++ constructors/bin/main.dart
@@ -1,12 +1,20 @@
+import 'model.dart' show A;
+
 void main() {
   A a = A();
-  A b = A();
-  print('${a.hashCode} ${b.hashCode}');
-  print(identical(a, b));
-}
-
-class A extends Object {
-  // String id;
-  // A({required this.id});
-
+  print(a.a);
+  print(a.b);
+  print(a.c);
+  a.d = 0;
+  print(a.d);
+  a.e = 0;
+  print(a.e);
+  print(a.f);
+  print(A.g);
+  A.h = 0;
+  print(A.h);
+  print(A.i);
+  A.j = 0;
+  print(A.j);
+  print(A.k);
 }
---
+++ constructors/bin/model.dart
@@ -0,0 +1,18 @@
+class A {
+  int? _primate;
+  int? a;
+  int? b = 1;
+
+  final int c = 2;
+
+  late int d;
+  late final int e;
+  late final int f = 10;
+
+  static int g = 6;
+  static late int h;
+  static late int i = 5;
+  static late final int j;
+
+  static const int k = 10;
+}
