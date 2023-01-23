de493731fb4c4070b82228537fc7684cc24e6089
remon <blackshadowsoftwareltd@gmail.com>
2023-01-24 00:17:49 +0600

    zeroX, zeroY, zeroZ constructor

--- constructors/bin/main.dart
+++ constructors/bin/main.dart
@@ -1,10 +1,12 @@
 import 'model.dart' show A;
 
 void main() {
-  A a = A(10, y: 20, z: 30);
   A zero = A.zero(10, y: 20); // Zero constructor
-  A json = A.fromJson({'x': 10, 'y': 20, 'z': 30}); // From JSON constructor
-  print(a.toString());
+  A zeroX = A.zeroX(x: 10); // Zero constructor
+  A zeroY = A.zeroY(y: 20); // Zero constructor
+  A zeroZ = A.zeroZ(z: 30); // Zero constructor
   print(zero.toString());
-  print(json);
+  print(zeroX.toString());
+  print(zeroY.toString());
+  print(zeroZ.toString());
 }
--- constructors/bin/model.dart
+++ constructors/bin/model.dart
@@ -1,13 +1,15 @@
 class A {
-  // Named constructor
-  A(this.x, {required this.y, required this.z});
+  A({
+    required this.x,
+    required this.y,
+    required this.z,
+  }); // must need this constructor for zeroX & zeroY constructors
 
   A.zero(this.x, {required this.y}) : z = 0; // Zero constructor
 
-  A.fromJson(Map<String, dynamic> json) // From JSON constructor
-      : x = json['x'],
-        y = json['y'],
-        z = json['z'];
+  A.zeroX({required int x}) : this(x: x, y: 0, z: 0); // ZeroX constructor
+  A.zeroY({required int y}) : this(x: 0, y: y, z: 0); // ZeroY constructor
+  A.zeroZ({required int z}) : this(x: 0, y: 0, z: z); // ZeroY constructor
 
   final int x;
   final int y;
