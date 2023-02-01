15214413967cb3428884baf61a38579ac3a442ee
Remon <blackshadowsoftwareltd@gmail.com>
2023-02-01 20:49:35 +0600

    method override & polymorphism

--- inheritances/bin/model.dart
+++ inheritances/bin/model.dart
@@ -5,9 +5,23 @@ class Animal {
 
   Animal.fromJson() : name = 'Jerry';
 
-  void whoAmI() {
-    print('I am an $name');
-  }
+  void whoAmI() => print('I am an $name');
+  void chase(Animal animal) => print('I am chasing a $animal');
+}
+
+class Mouse extends Animal {
+  Mouse() : super(name: 'Jerry');
+}
+
+class Cat extends Animal {
+  Cat() : super(name: 'Tom');
+
+  ///? Chase method can not override because chese method is not property of Mouse class.
+  ///? So follow the code below >> chase(covariant Mouse mouse) !! covariant keyword is used to override the method.
+  //! @override
+  //! void chase(Mouse mouse) => print('I am chasing a $animal');
+  @override
+  void chase(covariant Mouse mouse) => print('I am chasing a $mouse');
 }
 
 class Bird extends Animal {
