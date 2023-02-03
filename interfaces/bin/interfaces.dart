a5e01e99ef49ea2bb78476a46042c324eceb4017
Remon <blackshadowsoftwareltd@gmail.com>
2023-02-03 20:03:15 +0600

    external keyword for calling extended method

--- interfaces/bin/interfaces.dart
+++ interfaces/bin/interfaces.dart
@@ -2,9 +2,5 @@ import 'model.dart';
 
 void main() {
   final user = User();
-  print(user.users);
-  user.methodForA();
-  user.methodForB();
-  user.methodForC();
-  user.methodForD();
+  user.externalMethod('Hello World');
 }
--- interfaces/bin/model.dart
+++ interfaces/bin/model.dart
@@ -1,46 +1,8 @@
-abstract class UserRepository {
-  List<String> users = [];
-
-  void readFromLocal(List<String> users);
+class UserRepo {
+  external void externalMethod(String s); // body less method.
 }
 
-class A {
-  void methodForA() {}
+class User extends UserRepo {
+  @override
+  void externalMethod(String s) => print('External method called with $s');
 }
-
-class B {
-  void methodForB() {}
-}
-
-class C {
-  void methodForC() {}
-}
-
-class D {
-  void methodForD() {}
-}
-
-class User extends D implements A, B, C, UserRepository {
-  User() {
-    readFromLocal(_users);
-  }
-
-  @override
-  List<String> users = [];
-
-  @override
-  void readFromLocal(List<String> users) {
-    this.users.addAll(users);
-  }
-
-  @override
-  void methodForA() => print('method For A');
-  @override
-  void methodForB() => print('method For B');
-  @override
-  void methodForC() => print('method For C');
-  @override
-  void methodForD() => print('method For D');
-}
-
-const _users = ['user1', 'user2', 'user3', 'user4', 'user5'];
