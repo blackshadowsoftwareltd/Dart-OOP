5e3e8cbddba98d4ec72b6e3218ead62f5ac27449
Remon <blackshadowsoftwareltd@gmail.com>
2023-02-03 19:56:08 +0600

    implecet interface (not abstract)

--- interfaces/bin/interfaces.dart
+++ interfaces/bin/interfaces.dart
@@ -2,22 +2,8 @@ import 'model.dart';
 
 void main() {
   final user = User();
-
   print(user.users);
-
-  user.add('user6');
-
-  print(user.users);
-
-  user.find('user2');
-
-  print(user.users);
-
-  user.remove('user6');
-
-  print(user.users);
-
-  user.update(user.users.length - 1, 'lastUser');
-
-  print(user.users);
+  user.methodForA();
+  user.methodForB();
+  user.methodForC();
 }
