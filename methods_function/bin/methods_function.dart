a5561b5107e57a8fb089acdf0e159ffa1d34acdd
remon <blackshadowsoftwareltd@gmail.com>
2023-02-22 00:47:10 +0600

    complex method with generic T type

--- methods_function/bin/methods_function.dart
+++ methods_function/bin/methods_function.dart
@@ -1,17 +1,12 @@
 void main() {
-  ///?  do normal operation
-  print(doOparation(10.0, 5.0, (a, b) => a + b));
-
   ///?  do complex operation
-  final x = doComplexOparation(10.0, 5.0, (sum) => sum + 50);
+  final x = doComplexOparation(10.0, 5.0, operation: (sum) => sum + 50);
   print('Do something with the sum of (a+b) = : $x');
 }
 
-///?  do normal operation
-double doOparation(double a, double b, Function operation) => operation(a, b);
-
 ///?  do complex operation
-double doComplexOparation(
-        double a, double b, double Function(double) operation) =>
-    operation(add(100, 50));
-double add([double a = 10, double b = 5]) => a + b;
+T doComplexOparation<T extends num>(T a, T b,
+        {required T Function(T) operation}) =>
+    operation(add(a, b));
+
+T add<T extends num>(T a, T b) => a + b as T;
