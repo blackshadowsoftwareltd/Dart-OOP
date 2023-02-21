ae83e800db800f1c42d95a393aa876f117c652f6
remon <blackshadowsoftwareltd@gmail.com>
2023-02-21 13:35:28 +0600

    stream controller with stream subscription

--- streams/bin/streams.dart
+++ streams/bin/streams.dart
@@ -1,11 +1,17 @@
 import 'dart:async';
 
 void main() {
+  int counter = 1;
   final StreamController streamController = StreamController();
+  StreamSubscription streamSubscription = streamController.stream.listen(print);
+
   Timer.periodic(const Duration(seconds: 1), (timer) {
-    streamController.add(timer.tick);
-  });
-  streamController.stream.listen((event) {
-    print(event);
+    if (timer.tick > 5) {
+      timer.cancel();
+      streamController.close();
+      streamSubscription.cancel();
+    } else {
+      streamController.add(counter++);
+    }
   });
 }
