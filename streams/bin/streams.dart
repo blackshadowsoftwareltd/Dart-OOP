0d4b11eac29ed13cc6289fdd2bb5f526b64385be
remon <blackshadowsoftwareltd@gmail.com>
2023-02-21 13:40:14 +0600

    multiple Subscriptions on a single StreamController

--- streams/bin/streams.dart
+++ streams/bin/streams.dart
@@ -2,14 +2,17 @@ import 'dart:async';
 
 void main() {
   int counter = 1;
-  final StreamController streamController = StreamController();
-  StreamSubscription streamSubscription = streamController.stream.listen(print);
+  final StreamController streamController = StreamController<
+      int>.broadcast(); //? Broadcast Stream will give listen to multiple Stream Subscriptions on a single StreamController.
+  StreamSubscription subscription1 = streamController.stream.listen(print);
+  StreamSubscription subscription2 = streamController.stream.listen(print);
 
   Timer.periodic(const Duration(seconds: 1), (timer) {
     if (timer.tick > 5) {
       timer.cancel();
       streamController.close();
-      streamSubscription.cancel();
+      subscription1.cancel();
+      subscription2.cancel();
     } else {
       streamController.add(counter++);
     }
