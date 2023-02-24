6e59b3f4daaf5bb7ec2b663dd5a764e2e6606580
remon <blackshadowsoftwareltd@gmail.com>
2023-02-24 23:57:36 +0600

    enums init

---
+++ enums/bin/enums.dart
@@ -0,0 +1,5 @@
+import 'package:enums/enums.dart' as enums;
+
+void main(List<String> arguments) {
+  print('Hello world: ${enums.calculate()}!');
+}
--- enums/test/enums_test.dart
+++ enums/test/enums_test.dart
@@ -1,4 +1,4 @@
-import 'package:methods_function/methods_function.dart';
+import 'package:enums/enums.dart';
 import 'package:test/test.dart';
 
 void main() {
