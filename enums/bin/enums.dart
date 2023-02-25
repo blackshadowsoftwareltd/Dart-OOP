71b5fad505a72c37a7f3870a1b199b7f749d7382
remon <blackshadowsoftwareltd@gmail.com>
2023-02-25 10:29:50 +0600

    enum with mixin

--- enums/bin/enums.dart
+++ enums/bin/enums.dart
@@ -1,53 +1,32 @@
 void main() {
-  final person = Person(name: 'John', car: Car.lamborghini);
-  switch (person.car) {
-    case Car.tesla:
-      print('Tesla');
-      print(person.toString());
-      break;
-    case Car.bmw:
-      print('BMW');
-      print(person.toString());
-      break;
-    case Car.audi:
-      print('Audi');
-      print(person.toString());
-      break;
-    case Car.ford:
-      print('Ford');
-      print(person.toString());
-      break;
-    case Car.lamborghini:
-      print('Lamborghini');
-      print(person.toString());
-      break;
-    case Car.toyota:
-      print('Toyota');
-      print(person.toString());
-      break;
+  try {
+    Animal.cat.jump();
+    Animal.dog.jump();
+    Animal.hen.jump();
+    Animal.fish.jump();
+  } catch (e) {
+    print(e.toString());
   }
 }
 
-///? Person class
-class Person {
-  final String name;
-  final Car car;
-  const Person({required this.name, required this.car});
+enum Animal with CanJump {
+  cat(jumpHeight: 2),
+  dog(jumpHeight: 1.5),
+  hen(jumpHeight: 0.5),
+  fish(jumpHeight: 0);
+
   @override
-  String toString() => 'Person { name: $name, car: $car }';
+  final double jumpHeight;
+  const Animal({required this.jumpHeight});
 }
 
-///* Enums *///
-enum Car {
-  tesla(name: 'Tesla', color: 'Red', price: 100000),
-  bmw(name: 'BMW', color: 'Black', price: 50000),
-  audi(name: 'Audi', color: 'White', price: 40000),
-  ford(name: 'Ford', color: 'Blue', price: 30000),
-  lamborghini(name: 'Lamborghini', color: 'Yellow', price: 1000000),
-  toyota(name: 'Toyota', color: 'Green', price: 20000);
-
-  final String name;
-  final String color;
-  final double price;
-  const Car({required this.name, required this.color, required this.price});
+mixin CanJump {
+  double get jumpHeight;
+  void jump() {
+    if (jumpHeight < 1) {
+      throw Exception('Jump height is too low');
+    } else {
+      print('Jumped $jumpHeight meters high');
+    }
+  }
 }
