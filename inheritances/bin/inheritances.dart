29715a0798367dde22b4cf398ee934ea9fecf810
remon <blackshadowsoftwareltd@gmail.com>
2023-02-25 11:48:04 +0600

    super contructor

--- inheritances/bin/inheritances.dart
+++ inheritances/bin/inheritances.dart
@@ -1,7 +1,19 @@
-import 'model.dart';
-
 void main() {
-  final duck = Duck(name: 'Munchkin');
-  print('Duck name: ${duck.name}');
-  duck.whoAmI(); // I am a duck. because of @override in Duck class
+  final person1 = Person1();
+  final person2 = Person2(role: Role.editor);
 }
+
+class Employee {
+  final Role role;
+  const Employee({required this.role});
+}
+
+class Person1 extends Employee {
+  const Person1() : super(role: Role.admin); //? add role in initially
+}
+
+class Person2 extends Employee {
+  const Person2({required super.role}); //? required super constructor's role
+}
+
+enum Role { admin, editor, author, user }
