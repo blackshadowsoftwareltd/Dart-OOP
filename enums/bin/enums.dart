5622cc65c373195cd5b38d4e52a9335a2d8ae203
remon <blackshadowsoftwareltd@gmail.com>
2023-02-25 10:35:02 +0600

    extension of enum method

--- enums/bin/enums.dart
+++ enums/bin/enums.dart
@@ -1,32 +1,27 @@
 void main() {
   try {
-    Animal.cat.jump();
-    Animal.dog.jump();
-    Animal.hen.jump();
-    Animal.fish.jump();
+    Animal.cat
+      ..jump()
+      ..run();
+    Animal.dog
+      ..jump()
+      ..run();
+    Animal.hen
+      ..jump()
+      ..run();
   } catch (e) {
     print(e.toString());
   }
 }
 
-enum Animal with CanJump {
-  cat(jumpHeight: 2),
-  dog(jumpHeight: 1.5),
-  hen(jumpHeight: 0.5),
-  fish(jumpHeight: 0);
-
-  @override
-  final double jumpHeight;
-  const Animal({required this.jumpHeight});
+extension Jump on Animal {
+  void jump() => print('$this is Jumping...');
 }
 
-mixin CanJump {
-  double get jumpHeight;
-  void jump() {
-    if (jumpHeight < 1) {
-      throw Exception('Jump height is too low');
-    } else {
-      print('Jumped $jumpHeight meters high');
-    }
-  }
+enum Animal {
+  cat,
+  dog,
+  hen;
+
+  void run() => print('Running...');
 }
