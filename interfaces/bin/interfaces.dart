9eae8f65b320d6d7035940307d8a09be47ec92f2
remon <blackshadowsoftwareltd@gmail.com>
2023-02-25 12:26:59 +0600

    mixin

--- interfaces/bin/interfaces.dart
+++ interfaces/bin/interfaces.dart
@@ -1,11 +1,30 @@
-import 'model.dart' show D;
-
 void main() {
-  final model = D();
-  model.a();
-  model.b();
-  model.c();
-//========
-  model.bToA();
-  model.cToA();
+  var person = Person();
+  person.run(25);
+  person.jump(5);
+}
+
+///* class implements mixin
+class Person with MaxSpeed, CanRun, CanWalk {
+  @override
+  double speed; // speed must be override from MaxSpeed mixin
+
+  Person() : speed = 00;
+}
+
+///* mixin
+mixin MaxSpeed {
+  abstract double speed;
+}
+mixin CanRun on MaxSpeed {
+  void run(double speed) {
+    this.speed = speed;
+    print('$runtimeType is running with speed $speed');
+  }
+}
+mixin CanWalk on MaxSpeed {
+  void jump(double speed) {
+    this.speed = speed;
+    print('$runtimeType is walking with speed $speed');
+  }
 }
