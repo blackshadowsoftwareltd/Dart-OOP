4437b37237a57ebe2dca6de48f014230b8182cec
remon <blackshadowsoftwareltd@gmail.com>
2023-02-28 08:53:01 +0600

    hashCode in mixin. checking identical using Set

--- constructors/bin/main.dart
+++ constructors/bin/main.dart
@@ -1,11 +1,37 @@
-import 'model.dart' show Point;
-
 void main() {
-  final p = Point();
-  p.setX = 10;
-  p.setY = 20;
-  
+  final cats = <Dog>{
+    Dog(name: 'Modi', age: 80),
+    Dog(name: 'Joe Biden', age: 82),
+    Dog(name: 'Modi', age: 80),
+  };
+  cats.forEach(print);
+}
 
-  print('p + n = ${p.sum}');
-  print('p - n = ${p.diff}');
+class Dog with Pet {
+  @override
+  final int age;
+
+  @override
+  final String name;
+
+  @override
+  final PetType type;
+
+  const Dog({required this.name, required this.age}) : type = PetType.dog;
+}
+
+enum PetType { dog, cat }
+
+mixin Pet {
+  String get name;
+  PetType get type;
+  int get age;
+  @override
+  String toString() => 'Pet{name: $name, type: $type, age: $age}';
+
+  @override
+  int get hashCode => name.hashCode ^ type.hashCode ^ age.hashCode;
+
+  @override
+  bool operator ==(Object other) => other.hashCode == hashCode;
 }
