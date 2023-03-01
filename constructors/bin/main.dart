144414bd7cd193e6c088be100dcc9d767f73762e
remon <blackshadowsoftwareltd@gmail.com>
2023-03-01 08:39:09 +0600

    init

--- constructors/bin/main.dart
+++ constructors/bin/main.dart
@@ -1,8 +1,33 @@
 void main() {
+<<<<<<< HEAD
   print('Hello World' - 'World');
   print('Hello World' - 'Earth'); 
 }
 
 extension RemoveString on String {
   String operator -(String other) => replaceAll(other, '');
+=======
+  final father = FamilyMember(name: 'Dat');
+  final mother = FamilyMember(name: 'Mom');
+  final family = father + mother; // + operator comes from extension
+  print(family);
+}
+
+class FamilyMember {
+  final String name;
+  const FamilyMember({required this.name});
+  @override
+  String toString() => 'FamilyMember (name: $name)';
+}
+
+class Family {
+  final List<FamilyMember> members;
+  const Family({required this.members});
+  @override
+  String toString() => 'Family (members: $members)';
+}
+
+extension ToFamily on FamilyMember {
+  Family operator +(FamilyMember other) => Family(members: [this, other]);
+>>>>>>> 630faf4 (custom operator example 1)
 }
---
+++ operators/bin/operators.dart
@@ -0,0 +1,5 @@
+import 'package:operators/operators.dart' as operators;
+
+void main(List<String> arguments) {
+  print('Hello world: ${operators.calculate()}!');
+}
