95494609ddfb4d6fd1aa9db552c9e24790ea5f83
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-03-04 13:06:17 +0600

    custome perator (String - String)

--- constructors/bin/main.dart
+++ constructors/bin/main.dart
@@ -1,14 +1,8 @@
 void main() {
-  print(sum(5, 10));
+  print('Hello World' - 'World');
+  print('Hello World' - 'Earth'); 
 }
 
-int sum(int? a, int? b) => a + b;
-
-extension NullableAdd<T extends num> on T? {
-  T operator +(T? other) {
-    if (this == null && other == null) return 0 as T;
-    if (this == null) return other as T;
-    if (other == null) return this as T;
-    return this! + other as T;
-  }
+extension RemoveString on String {
+  String operator -(String other) => replaceAll(other, '');
 }
