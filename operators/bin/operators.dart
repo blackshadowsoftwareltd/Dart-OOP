cdd1bd0f27e0cf176b4ce5e13808352734d61f03
remon <blackshadowsoftwareltd@gmail.com>
2023-03-05 10:40:30 +0600

    _

--- operators/bin/operators.dart
+++ operators/bin/operators.dart
@@ -1,5 +1,1 @@
-import 'package:operators/operators.dart' as operators;
-
-void main(List<String> arguments) {
-  print('Hello world: ${operators.calculate()}!');
-}
+void main() {}
--- operators/test/operators_test.dart
+++ operators/test/operators_test.dart
@@ -1,8 +1,1 @@
-import 'package:operators/operators.dart';
-import 'package:test/test.dart';
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
