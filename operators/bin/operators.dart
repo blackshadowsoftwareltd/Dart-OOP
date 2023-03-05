598dde607984335da1582b119b71eb2fc196c02e
remon <blackshadowsoftwareltd@gmail.com>
2023-03-05 10:53:33 +0600

    operator. remove element from another list using custom element

--- operators/bin/operators.dart
+++ operators/bin/operators.dart
@@ -1,1 +1,7 @@
-void main() {}
+void main() {
+  print([1, 2, 3, 4, 5] - [3, 5]);
+}
+
+extension Remove<T> on Iterable<T> {
+  Iterable<T> operator -(Iterable<T> other) => where((e) => !other.contains(e));
+}
