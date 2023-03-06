30406461db960b320dd614d3c42578392989b6fb
remon <blackshadowsoftwareltd@gmail.com>
2023-03-06 08:31:25 +0600

    custom operator for add, remove, repeat on Map<K,V>

--- operators/bin/operators.dart
+++ operators/bin/operators.dart
@@ -1,8 +1,25 @@
 void main() {
-  print([1, 2, 3, 4, 5] - [3, 5]);
-  print(['a', 'b', 'c', 'd', 'e'] - ['c', 'e']);
+  ///! the age key is overwritten by the second map using + operator
+  final mapAdd = {'name': 'John', 'age': 30} + {'age': 40, 'city': 'New York'};
+  print(mapAdd);
+
+  ///! the city is matched and removed from the map but the age is not matched so it is not removed
+  final mapRemove = mapAdd - {'age': 20, 'city': 'New York'};
+  print(mapRemove);
+
+  ///? the * operator is used to repeat the map 3 times
+  print(mapRemove * 3);
 }
 
-extension Remove<T> on Iterable<T> {
-  Iterable<T> operator -(Iterable<T> other) => where((e) => !other.contains(e));
+extension MapOperations<K, V> on Map<K, V> {
+  ///! the + operator is used to merge two maps
+  Map<K, V> operator +(Map<K, V> other) => {...this, ...other};
+
+  ///! the - operator is used to remove the matching keys and values from the map
+  Map<K, V> operator -(Map<K, V> other) => {...this}..removeWhere(
+      (key, value) => other.containsKey(key) && other[key] == value);
+
+  Iterable<Map<K, V>> operator *(int times) sync* {
+    for (int i = 0; i < times; i++) yield this;
+  }
 }
