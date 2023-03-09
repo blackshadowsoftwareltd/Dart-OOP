005d8946c9fee788d3c6a27ea60431aa2c130698
remon <blackshadowsoftwareltd@gmail.com>
2023-03-10 00:48:27 +0600

    extension override

--- extensions/bin/extensions.dart
+++ extensions/bin/extensions.dart
@@ -1,1 +1,7 @@
-void main() {}
+import 'override.dart';
+
+void main() {
+  final p = Person(name: 'John', age: 20);
+  print(PersonInfo(p).userInfo); // extension contain same getter
+  print(PersonDescription(p).description); // extension contain same getter
+}
