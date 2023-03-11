92cfeb7b0d622977814be465cf8b6a2ca442cae5
blackshadowdoftwareltd <blackshadowsoftwareltd.com>
2023-03-11 11:52:02 +0600

    custom return type extends num

--- generics/bin/generics.dart
+++ generics/bin/generics.dart
@@ -1,25 +1,7 @@
-import 'model.dart'
-    show Cart, Product, User, cartsList, productsList, usersList;
-
 void main() {
-  final users = Reusable<User>();
-  users.addAll(usersList);
-  print(users.list);
-  final products = Reusable<Product>();
-  products.addAll(productsList);
-  print(products.list);
-  final carts = Reusable<Cart>();
-  carts.addAll(cartsList);
-  print(carts.list);
+  final number = add();
+  print(number);
+  print(number.runtimeType);
 }
 
-class Reusable<T> {
-  final List<T> _list = [];
-  List<T> get list => _list;
-  void add(T item) => _list.add(item);
-  void addAll(List<T> items) => _list.addAll(items);
-  void remove(T item) => _list.remove(item);
-  void removeAt(int index) => _list.removeAt(index);
-  void removeAll() => _list.clear();
-  int length() => _list.length;
-}
+CustomNum add<CustomNum extends num>() => 0 as CustomNum;
