9243df61cd06a6b992f633a3c7c6fb4889593664
blackshadowdoftwareltd <blackshadowsoftwareltd.com>
2023-03-11 12:50:02 +0600

    <Left,Right> generic on <L,R>

--- generics/bin/generics.dart
+++ generics/bin/generics.dart
@@ -1,7 +1,36 @@
 void main() {
-  final number = add();
-  print(number);
-  print(number.runtimeType);
+  const tupleInt = Tuple(left: 1, right: 2);
+  print(tupleInt);
+  print('Type: ${tupleInt.left.runtimeType} and ${tupleInt.right.runtimeType}');
+  print('Swaped ${tupleInt.swap()}');
+
+  const tupleString = Tuple(left: 'left', right: 'right');
+  print(tupleString);
+  print(
+      'Type: ${tupleString.left.runtimeType} and ${tupleString.right.runtimeType}');
+  print('Swaped ${tupleString.swap()}');
+
+  final swaped = tupleString.swap();
+  print(swaped); //? swaped is Tuple<String, String> .. <R,L>
+
+  print(tupleInt.sum); //? sum will be work if both are num type.
 }
 
-CustomNum add<CustomNum extends num>() => 0 as CustomNum;
+///? Menttion generic type. otherwise it will be dynamic type.
+extension<L, R> on Tuple<L, R> {
+  Tuple<R, L> swap() => Tuple(left: right, right: left); //? Swaped so=> <R, L>
+}
+
+///? Now we can sum if both are num (int/double) type.
+extension<L extends num, R extends num> on Tuple<L, R> {
+  num get sum => left + right;
+}
+
+class Tuple<L, R> {
+  final L left;
+  final R right;
+  const Tuple({required this.left, required this.right});
+
+  @override
+  String toString() => 'Tuple(left: $left, right: $right)';
+}
