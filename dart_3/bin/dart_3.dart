0fb42426e85d67b46a72840e4ac07d1bfde1aa81
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-05-25 09:40:32 +0600

    dart 3 init

---
+++ dart_3/bin/dart_3.dart
@@ -0,0 +1,5 @@
+import 'package:dart_3/dart_3.dart' as dart_3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_3.calculate()}!');
+}
---
+++ dart_3/test/dart_3_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_3/dart_3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
